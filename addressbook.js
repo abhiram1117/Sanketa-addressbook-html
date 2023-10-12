d2fa9068e22e6d53a7ce49e5651040509afcf36f
abhiram1117 <143382378+abhiram1117@users.noreply.github.com>
2023-10-12 15:11:12 +0530

    Add files via upload

--- addressbook.js
+++ addressbook.js
@@ -1,20 +1,21 @@
 // displaying Employee details
 
 let employees = [
-    { profilePhoto: 'images/dp1.png', firstName: 'Anthony',lastName: 'Morris', jobTitle: 'SharePoint Practice Head', department: 'IT', profileURL: '1.html', location:'Seattle' },
-    { profilePhoto: 'images/dp2.png', firstName: 'Helen', lastName: ' Zimmerman', jobTitle: 'Operatons Manager', department: 'IT', profileURL: '2.html', location: 'Seattle' },
-    { profilePhoto: 'images/dp3.png', firstName: 'Joanthon', lastName: ' Smith', jobTitle: 'Product Manager', department: 'IT', profileURL: '3.html', location: 'Seattle' },
-    { profilePhoto: 'images/dp4.png', firstName: 'Tami', lastName: ' Hopkins', jobTitle: 'Lead Engineer', department: 'IT', profileURL: '4.html', location: 'Seattle' },
-    { profilePhoto: 'images/dp5.png', firstName: 'Franklin', lastName: ' Humark', jobTitle: 'Network Engineer', department: 'IT', profileURL: '5.html', location: 'Seattle' },
-    { profilePhoto: 'images/dp6.png', firstName: 'Angela', lastName: ' Bailey', jobTitle: 'Talent Manager', department: 'HR', profileURL: '6.html', location: 'Seattle' },
-    { profilePhoto: 'images/dp7.png', firstName: 'Robert', lastName: ' Mitchell', jobTitle: 'Software Engineer', department: 'IT', profileURL: '7.html', location: 'Seattle' },
-    { profilePhoto: 'images/dp8.png', firstName: 'Olivia', lastName: ' Watson', jobTitle: 'UI Designer', department: 'UX', profileURL: '8.html', location: 'India' },
+    { id: '1', profilePhoto: 'images/dp1.png', firstName: 'Anthony', lastName: 'Morris', jobTitle: 'SharePoint Practice Head', department: 'IT', profileURL: '1.html', location: 'Seattle'},
+    { id: '2', profilePhoto: 'images/dp2.png', firstName: 'Helen', lastName: ' Zimmerman', jobTitle: 'Operatons Manager', department: 'IT', profileURL: '2.html', location: 'Seattle' },
+    { id: '3', profilePhoto: 'images/dp3.png', firstName: 'Joanthon', lastName: ' Smith', jobTitle: 'Product Manager', department: 'IT', profileURL: '3.html', location: 'Seattle' },
+    { id: '4', profilePhoto: 'images/dp4.png', firstName: 'Tami', lastName: ' Hopkins', jobTitle: 'Lead Engineer', department: 'IT', profileURL: '4.html', location: 'Seattle' },
+    { id: '5', profilePhoto: 'images/dp5.png', firstName: 'Franklin', lastName: ' Humark', jobTitle: 'Network Engineer', department: 'IT', profileURL: '5.html', location: 'Seattle' },
+    { id: '6', profilePhoto: 'images/dp6.png', firstName: 'Angela', lastName: ' Bailey', jobTitle: 'Talent Manager', department: 'HR', profileURL: '6.html', location: 'Seattle' },
+    { id: '7', profilePhoto: 'images/dp7.png', firstName: 'Robert', lastName: ' Mitchell', jobTitle: 'Software Engineer', department: 'IT', profileURL: '7.html', location: 'Seattle' },
+    { id: '8', profilePhoto: 'images/dp8.png', firstName: 'Olivia', lastName: ' Watson', jobTitle: 'UI Designer', department: 'UX', profileURL: '8.html', location: 'India' },
 ];
 
 const employeeList = document.getElementById('employeeList');
 const editModal = document.getElementById('editEmployeeModal');
 const closeEditModalButton = document.getElementById('closeEditModal');
 const employeeCards = document.querySelectorAll('.employee-card');
+let selectedEmployee;
 
 function displayEmployees(employeeData) {
     employeeList.innerHTML = "";
@@ -38,37 +39,37 @@ function displayEmployees(employeeData) {
 
 function openEditEmployeeModal(employee) {
     editModal.style.display = 'block';
-
+    selectedEmployeeId = employee.id;
     document.getElementById('editFirstName').value = employee.firstName;
     document.getElementById('editLastName').value = employee.lastName;
     document.getElementById('editJobTitle').value = employee.jobTitle;
     document.getElementById('editDepartment').value = employee.department;
-    document.getElementById('editLocation').value = employee.location;
-    document.getElementById('saveEdit').addEventListener('click', function () {
-        saveEmployeeEdits(employee);
-    });
+    
 }
+
+document.getElementById('saveEdit').addEventListener('click', function (event) {
+    event.preventDefault();
+    saveEmployeeEdits();
+});
 
 function closeEditEmployeeModal() {
     editModal.style.display = 'none';
 }
-
-function saveEmployeeEdits(employee) {
-    const editedFirstName = document.getElementById('editFirstName').value;
-    const editedLastName = document.getElementById('editLastName').value;
-    const editedJobTitle = document.getElementById('editJobTitle').value;
-    const editedDepartment = document.getElementById('editDepartment').value;
-    const editedLocation = document.getElementById('editLocation').value;
-
-    employee.firstName = editedFirstName;
-    employee.lastName = editedLastName;
-    employee.jobTitle = editedJobTitle;
-    employee.department = editedDepartment;
-    employee.location = editedLocation;
-
+    
+function saveEmployeeEdits(event) {
+    let index = employees.findIndex(emp => emp.id == selectedEmployeeId);
+    let employee = employees[index]
+    employee.firstName = document.getElementById('editFirstName').value;
+    employee.lastName = document.getElementById('editLastName').value;
+    employee.jobTitle = document.getElementById('editJobTitle').value;
+    employee.department = document.getElementById('editDepartment').value;
+    
     closeEditEmployeeModal();
+    console.log("Before Displaying:", employees);
     displayEmployees(employees);
+    console.log("After Displaying:", employees);
 }
+
 closeEditModalButton.addEventListener('click', closeEditEmployeeModal);
 
 
